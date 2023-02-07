64e9dc93a82773e3d34410cbacd57bd757a9bf71
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-02-07 14:24:22 -0800

    added route protections

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {User,Chirp} = require('../models');
+const bcrypt = require("bcrypt");
 
 router.get("/",(req,res)=>{
    User.findAll().then(userData=>{
@@ -21,14 +22,37 @@ router.get("/:id",(req,res)=>{
    })
 })
 
-
 router.post("/",(req,res)=>{
     console.log(req.body);
    User.create({
     email:req.body.email,
     password:req.body.password
    }).then(userData=>{
+    req.session.userId = userData.id;
+    req.session.userEmail = userData.email;
     res.json(userData)
+   }).catch(err=>{
+    console.log(err);
+    res.status(500).json({msg:"oh noes!",err})
+   })
+})
+router.post("/login",(req,res)=>{
+   User.findOne({
+   where:{
+    email:req.body.email
+   }
+   }).then(userData=>{
+    if(!userData){
+        return res.status(401).json({msg:"incorrect email or password"})
+    } else {
+        if(bcrypt.compareSync(req.body.password,userData.password)){
+            req.session.userId = userData.id;
+            req.session.userEmail = userData.email;
+            return res.json(userData)
+        } else {
+            return res.status(401).json({msg:"incorrect email or password"})
+        }
+    }
    }).catch(err=>{
     console.log(err);
     res.status(500).json({msg:"oh noes!",err})
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 // const exphbs = require('express-handlebars');
 const allRoutes = require('./controllers');
 
 const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Sets up the Express App
 // =============================================================
@@ -13,17 +13,19 @@ const PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
 const { User,Chirp} = require('./models');
 
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge:1000*60*60*2
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 // // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -38,6 +40,22 @@ app.use(express.json());
 app.use(allRoutes);
 app.get("/",(req,res)=>{
     res.send("hello welcome to chirpy!")
+})
+
+app.get("/sessions",(req,res)=>{
+    res.json(req.session)
+})
+app.get("/favecolor/:color",(req,res)=>{
+    req.session.favColor = req.params.color
+    res.json(req.session)
+})
+
+app.get("/secretclub",(req,res)=>{
+    if(req.session.userId){
+        return res.send(`welcome to the secret club, ${req.session.userEmail}`)
+    } else {
+        res.status(403).json({msg:"login first to join the club!"})
+    }
 })
 sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
