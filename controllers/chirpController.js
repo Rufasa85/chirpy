4ca9404d6da1650626cfce59aee35001237ef172
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-02-08 10:53:12 -0800

    adding delete route

--- controllers/chirpController.js
+++ controllers/chirpController.js
@@ -23,11 +23,14 @@ router.get("/:id",(req,res)=>{
 
 //TODO: protect this route so only logged in users can chirp
 router.post("/",(req,res)=>{
-    console.log(req.body);
+   if(!req.session.userId){
+      return res.status(403).json({msg:"login first post"})
+   }
+   console.log(req.body);
    Chirp.create({
     chirp:req.body.chirp,
     //TODO: read userid from session data instead of from req.body
-    UserId:req.body.UserId
+    UserId:req.session.userId
    }).then(chirpData=>{
     res.json(chirpData)
    }).catch(err=>{
@@ -37,5 +40,31 @@ router.post("/",(req,res)=>{
 })
 
 //TODO: BONUS: add a protected route to delete a chirp (/api/chirps/:id) so that only the user who created the chirp can delete it
+router.delete("/:id",(req,res)=>{
+   if(!req.session.userId){
+      return res.status(403).json({msg:"login first post"})
+   }
+   console.log(req.body);
+   Chirp.findByPk(req.params.id).then(chirpData=>{
+      if(!chirpData){
+         return res.status(404).json({msg:"no such chirp"})
+      } else if(chirpData.UserId!== req.session.userId){
+         return res.status(403).json({msg:"not your chirp!"})
+      }
+      Chirp.destroy({
+       where:{
+          id:req.params.id,
+       }
+      }).then(chirpData=>{
+        res.json(chirpData)
+       }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:"oh noes!",err})
+       })
+   }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:"oh noes!",err})
+   })
+})
 
 module.exports = router;
